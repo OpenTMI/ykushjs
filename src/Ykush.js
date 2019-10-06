344cdbcfcfeb328956306198f720ca6b8b6ea6f2
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-10-06 15:09:55 +0300

    more tests

--- src/Ykush.js
+++ src/Ykush.js
@@ -3,8 +3,7 @@ const execa = require('execa');
 const _ = require('lodash');
 const invariant = require('invariant');
 
-
-module.exports = class Ykush {
+class Ykush {
     constructor(serialNumber, {logger}) {
         this._serialNumber = serialNumber;
         this.logger = logger;
@@ -12,14 +11,10 @@ module.exports = class Ykush {
     }
 
     get _ykushcmd() { // eslint-disable-line class-methods-use-this
-        /**
+    /**
          * get ykushcmd - should be available in $PATH
          */
         return 'ykushcmd';
-    }
-
-    static get execa() {
-        return execa;
     }
 
     get _args() { // eslint-disable-line class-methods-use-this
@@ -29,7 +24,7 @@ module.exports = class Ykush {
     async _runYkushCmd(args) {
         const cmd = this._ykushcmd;
         this.logger.debug(`ykush cmd: 'cmd ${args.join(' ')}'`);
-        const {stdout} = await ResourceYkush.execa(cmd, args);
+        const {stdout} = await Ykush.execa(cmd, args);
         this.logger.silly(`stdout: ${stdout}`);
         return {};
     }
@@ -46,7 +41,7 @@ module.exports = class Ykush {
 
     _validateChannel(channel) {
         invariant(channel >= 0, 'invalid channel');
-        invariant(channel < this._channelCount, `allowed channels are 0..${_channelCount}`);
+        invariant(channel < this._channelCount, `allowed channels are 0..${this._channelCount}`);
     }
 
     async powerOn({channel}) {
@@ -65,11 +60,14 @@ module.exports = class Ykush {
         const args = [...this._args, '-l'];
         const {stdout} = await this._runYkushCmd(args);
         const str = stdout.slice(stdout.indexOf('[')).trim();
-        if (str == '') {
+        if (!str) {
             return [];
         }
         const arr = JSON.parse(str);
         const ykushes = arr.map(id => ({id}));
         return ykushes;
     }
-};
+}
+
+Ykush.execa = execa;
+module.exports = Ykush;
--- test/test_ykush.js
+++ test/test_ykush.js
@@ -1,6 +1,7 @@
 // 3rd party modules
 const _ = require('lodash');
 const chai = require('chai');
+const {stub} = require('sinon');
 const chaiAsPromised = require('chai-as-promised');
 // application modules
 const {Ykush} = require('../src');
@@ -9,10 +10,37 @@ chai.use(chaiAsPromised);
 const {expect} = chai;
 
 
-describe('Ykush', () => {
-  const logger = {debug: () => {}};
-  it('is ok', () => {
-    const obj = new Ykush('123', {logger});
-    expect(obj).to.be.ok;
+describe('Ykush', function () {
+    const logger = {debug: () => {}, silly: () => {}};
+    beforeEach(function () {
+        stub(Ykush, 'execa');
+        Ykush.execa.resolves({stdout: ''});
+    });
+    afterEach(function () {
+        Ykush.execa.restore();
+    });
+    it('is ok', function () {
+        const obj = new Ykush('123', {logger});
+        expect(obj).to.be.ok;
+    });
+    it('powerAllOn', async function () {
+        const ykush = new Ykush('123', {logger});
+        await ykush.powerAllOn();
+        expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-u'])).to.be.true;
+    });
+  it('powerAllOff', async function () {
+    const ykush = new Ykush('123', {logger});
+    await ykush.powerAllOff();
+    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d'])).to.be.true;
+  });
+  it('powerOn', async function () {
+    const ykush = new Ykush('123', {logger});
+    await ykush.powerOn({channel: 1});
+    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-u', '1'])).to.be.true;
+  });
+  it('powerOff', async function () {
+    const ykush = new Ykush('123', {logger});
+    await ykush.powerOn({channel: 1});
+    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d', '1'])).to.be.true;
   });
 });
