df1cb90893de7a4a9f4532fd52fd9f8d4fa29275
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-11-22 20:54:52 +0200

    test fixes

--- test/test_ykush.js
+++ test/test_ykush.js
@@ -1,5 +1,4 @@
 // 3rd party modules
-const _ = require('lodash');
 const chai = require('chai');
 const {stub} = require('sinon');
 const chaiAsPromised = require('chai-as-promised');
@@ -28,19 +27,19 @@ describe('Ykush', function () {
         await ykush.powerAllOn();
         expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-u'])).to.be.true;
     });
-  it('powerAllOff', async function () {
-    const ykush = new Ykush('123', {logger});
-    await ykush.powerAllOff();
-    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d'])).to.be.true;
-  });
-  it('powerOn', async function () {
-    const ykush = new Ykush('123', {logger});
-    await ykush.powerOn({channel: 1});
-    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-u', '1'])).to.be.true;
-  });
-  it('powerOff', async function () {
-    const ykush = new Ykush('123', {logger});
-    await ykush.powerOn({channel: 1});
-    expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d', '1'])).to.be.true;
-  });
+    it('powerAllOff', async function () {
+        const ykush = new Ykush('123', {logger});
+        await ykush.powerAllOff();
+        expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d'])).to.be.true;
+    });
+    it('powerOn', async function () {
+        const ykush = new Ykush('123', {logger});
+        await ykush.powerOn({channel: 1});
+        expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-u', '1'])).to.be.true;
+    });
+    it('powerOff', async function () {
+        const ykush = new Ykush('123', {logger});
+        await ykush.powerOff({channel: 1});
+        expect(Ykush.execa.calledOnceWith('ykushcmd', ['-s', '123', '-d', '1'])).to.be.true;
+    });
 });
