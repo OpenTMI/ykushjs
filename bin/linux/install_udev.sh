fc9c1c64d75eaab9654f1dcc69b3c78a857cc290
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-11-24 09:33:33 +0200

    wip

--- bin/linux/install_udev.sh
+++ bin/linux/install_udev.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Simple installation script for ykush udev rules
 
+# exit immediately if fail
+set -e
+
 
 if [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
     echo "This script is only for linux"
@@ -16,5 +19,5 @@ fi
 cp 99-ykush.rules /etc/udev/rules.d/99-ykush.rules
 chmod 644 /etc/udev/rules.d/99-ykush.rules
 usermod -a -G dialout $USER
-udevadm control --reload
+udevadm control --reload-rules && udevadm trigger
 echo "Ready"
